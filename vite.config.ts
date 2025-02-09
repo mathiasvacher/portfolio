80e5c2c1511a4539917949e4b16dd4eef3722d96
mathiasvacher <vachermathias42@gmail.com>
2025-02-09 15:36:39 +0100

    Ajustement CSS section realisation

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio/dist/', // Assure-toi que c'est bien ton nom de repo GitHub
 })
