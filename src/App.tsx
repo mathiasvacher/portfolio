47ce300753a6e6853c2c4a33bd3e62aee4be1c48
mathiasvacher <vachermathias42@gmail.com>
2025-02-09 14:15:05 +0100

    deploy commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import './scss/style.scss';
-import CursorEffect from './components/cursoreffect.tsx';
-import HeroEffect from './components/heroeffect.tsx';
-import Header from './components/header.tsx'; 
-import Hero from './components/hero.tsx'; 
-import Realisations from './components/realisations.tsx'; 
-import Experiences from './components/experiences.tsx'; 
-import Formations from './components/formations.tsx'; 
-import Footer from './components/footer.tsx'; 
-import Competences from './components/competences.tsx';
+import "./scss/style.scss";
+import CursorEffect from "./components/cursoreffect.tsx";
+import HeroEffect from "./components/heroeffect.tsx";
+import Header from "./components/header.tsx";
+import Hero from "./components/hero.tsx";
+import Realisations from "./components/realisations.tsx";
+import Experiences from "./components/experiences.tsx";
+import Formations from "./components/formations.tsx";
+import Footer from "./components/footer.tsx";
+import Competences from "./components/competences.tsx";
 
 function App() {
-
   return (
     <main>
-      {/* <CursorEffect /> */}
-      {/* <HeroEffect /> */}
+      <CursorEffect />
+      <HeroEffect />
       <Header />
       <Hero />
       <Realisations />
@@ -23,8 +22,7 @@ function App() {
       <Competences />
       <Footer />
     </main>
-
-  )
+  );
 }
 
-export default App
+export default App;
